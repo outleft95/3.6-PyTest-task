1e593e2db18ea625c038a129bcf4d1c5788d7059
Sergei Bakaikin <seba0217@netcracker.com>
2024-11-10 01:52:05 +0400

    add files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 3.6-PyTest-task
+Contains conftest.py and test_items.py
---
+++ test_items.py
@@ -0,0 +1,14 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_present_basket_button(browser):
+    browser.get(link) 
+    time.sleep(10)
+    element = browser.find_elements(By.CSS_SELECTOR, ".basket-mini .btn")
+    assert element, 'no element'
